779524e01120b9597dda10bfd8c0b4f70607cc9f
Garth Scaysbrook <brooksy4503@gmail.com>
2025-05-27 21:17:48 +1000

    Update configuration files: add image service entrypoint in astro.config.mjs, enable unsafe permissions in .npmrc, and adjust package.json for pnpm settings while removing unused dependencies from pnpm-lock.yaml.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	image: {
+		service: {
+			entrypoint: 'astro/assets/services/noop',
+		}
+	},
 	integrations: [
 		starlight({
 			title: 'ChatLima Documentation',
